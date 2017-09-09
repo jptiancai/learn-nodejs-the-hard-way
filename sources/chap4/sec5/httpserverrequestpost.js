a4f75713cf0e1a79e61780b70fdf9b5a7c30d679
jptiancai <mail_jzy@163.com>
2017-09-09 11:02:53 +0800

    add sources

--- sources/chap4/sec5/httpserverrequestpost.js
+++ sources/chap4/sec5/httpserverrequestpost.js
@@ -7,6 +7,10 @@ var util = require('util');
 http.createServer(function(req, res) {
   var post = '';
 
+/**
+ * 不要在真正的生产应用中使用上面这种简单的方法来获取 POST 请
+ *求，因为它有严重的效率问题和安全问题，这只是一个帮助你理解的示例。
+ */
   req.on('data', function(chunk) {
     post += chunk;
   });
